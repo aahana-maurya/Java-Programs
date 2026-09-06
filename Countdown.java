7d623d11bfc181b7480ff8655f07c631355263c3
aahana-maurya <aahanamaurya9@gmail.com>
2026-09-06 16:29:10 +0530

    Add New Year Countdown

---
+++ Countdown.java
@@ -0,0 +1,16 @@
+public class ForLoop{
+    public static void main(String[] args) throws InterruptedException {
+
+        //For Loop
+
+        int start =10;
+
+        for(int i = start; i > 0 ; i--){
+            System.out.println(i);
+            Thread.sleep(1000);
+        }
+
+        System.out.println("HAPPY NEW YEAR!!"); 
+
+    }
+}
